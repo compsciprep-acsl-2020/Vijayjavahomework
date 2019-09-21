8af2340680ce8c8f6ac2c08d73e8af999b6d0cdd
vijay <vj.dami@gmail.com>
2019-09-21 16:25:35 -0400

    second

--- vijay/src/HelloWorld.java
+++ vijay/src/HelloWorld.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-system.out.print(helloworld);
+//Scanner scan = new Scanner(System.in);
+        int value = 5;
+        int ab= 0;
+        int ac=0;
+        int ad=0;
+        int ae=0;
+        int af=0;
+        int ag=0;
+        int am=0;
+        int az=0;
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        int e = 0;
+    x= value%2;
+    ab=value/2;
+    y= ab % 2;
+    ac = ab/2;
+    z = ac % 2;
+    ad= ac/2;
+    a = ad % 2;
+    ae = ad/2;
+    b = ae % 2;
+    af= ae/2;
+    c = af % 2;
+    ag= af/2;
+    d= ag % 2;
+    am=ag/2;
+    e = am %2;
+
+    System.out.println(e+""+d+""+c+""+b+""+a+""+z+""+y+""+x);
+
+
+
+
+
+
+
     }
 }
