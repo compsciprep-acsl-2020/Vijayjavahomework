e32600ff9946cad9c7a7df84f17f7153947a2a30
vijay <vj.dami@gmail.com>
2019-10-05 14:49:25 -0400

    second

---
+++ vijay/src/FarenheighttoCelcius.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;public class FarenheighttoCelcius {
+
+    public static double ctof( double x)
+    {
+        x = (9.0/5.0 * x) - (9.0/5.0 * 32);
+        return x;
+    }
+    public static double ftoc( double y)
+    {
+        y = (5.0/9.0*y) + 32;
+        return y;
+    }
+public static void main(String[] args )
+{
+   Scanner s = new Scanner(System.in);
+   System.out.println("Enter the number");
+   double num = s.nextDouble();
+   System.out.println( "Is the number farenheight or celcius. Please type f or c non capitalized" );
+   String a = s.next();
+   switch(a) {
+       case "f":
+           double z = ftoc(num);
+           System.out.println(z);
+           break;
+
+
+       case "c":
+           double y = ctof(num);
+           System.out.println(y);
+           break;
+
+      // default:
+          // System.out.println("try again");
+   }
+
+
+
+   }
+
+}
+
+
---
+++ vijay/src/Fibbonachi.java
@@ -0,0 +1,8 @@
+public class Fibbonachi {
+    public static void main(String[] args ) {
+        int sum;
+        int a = 1;
+        int b = 2;
+        for( int i = 1)
+    }
+}
