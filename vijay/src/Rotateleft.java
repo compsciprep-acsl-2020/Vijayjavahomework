3dc65ff547a1f899097c13d75733484b6be65dda
CrookedChange <vj.dami@gmail.com>
2019-11-09 15:05:58 -0500

    a

---
+++ vijay/src/Rotateleft.java
@@ -0,0 +1,12 @@
+public class Rotateleft {
+    public static void main(String[] args ) {
+        int array[]= {1,2,3};
+        array[0]=array[1];
+        for( int i=1; i++; i<=3)
+        {
+            System.out.println( array[i]);
+        }
+    }
+
+
+}
---
+++ vijay/src/idAndZipcode.java
@@ -0,0 +1,7 @@
+public class idAndZipcode {
+    File    file = new File("something.txt");
+    Scanner scan = new Scanner( file);
+    int num, square;
+
+    num = scan.nextline();
+}
