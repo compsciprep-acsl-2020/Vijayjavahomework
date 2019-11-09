ec75afa54067784a4cee47657e8c08dfe79a3f6e
CrookedChange <vj.dami@gmail.com>
2019-11-09 15:07:21 -0500

    a

--- vijay/src/Rotateleft.java
+++ vijay/src/Rotateleft.java
@@ -2,7 +2,7 @@ public class Rotateleft {
     public static void main(String[] args ) {
         int array[]= {1,2,3};
         array[0]=array[1];
-        for( int i=1; i++; i<=3)
+        for( int i=1; i<=3; i++)
         {
             System.out.println( array[i]);
         }
