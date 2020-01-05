631f21f96347e6f3dc7ade9e99ae28b134c330a1
CrookedChange <vj.dami@gmail.com>
2020-01-05 12:28:56 -0500

    a

---
+++ vijay/src/MathmaticExpresssion.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+import java.util.Scanner;
+public class MathmaticExpresssion {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String a = s.nextint
+    }
+
+}
